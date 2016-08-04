382505950a4d7cd77725c967dbd6041fe92eb0d0
Student-003 <student-003@fexli-itss-macbook-pro.local>
2016-08-04 16:16:18 +0530

    Initial Commit

---
+++ assign6/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  assign6
+//
+//  Created by Student-003 on 04/08/16.
+//  Copyright © 2016 Felix-ITs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
